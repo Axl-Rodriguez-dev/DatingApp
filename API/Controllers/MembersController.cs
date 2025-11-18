0a89c10006937b48488c61a128e1df24acac053f
Axl-Rodriguez-dev <axl.rodriguez0824@gmail.com>
2025-11-18 07:45:57 -0500

    [AR] - Proyecto

---
+++ API/Controllers/MembersController.cs
@@ -0,0 +1,25 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class MembersController(AppDbContext context) : BaseApiController
+    {
+        [HttpGet] // url/api/members
+        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+        {
+            var members = await context.Users.ToListAsync();
+            return members;
+        }
+
+        [HttpGet("{id}")] // url/api/members/axl-id
+        public async Task<ActionResult<AppUser>> GetMember(string id)
+        {
+            var member = await context.Users.FindAsync(id);
+            if (member == null) return NotFound();
+            return member;
+        }
+    }
+}
